7945cf01bdf0d1fd5f52639d28b8868066164439
lucas mezuraro <lucaspmezuraro@gmail.com>
2021-07-24 14:31:54 -0300

    hello controller

---
+++ src/main/java/br/com/rodz/input/HelloController.java
@@ -0,0 +1,17 @@
+package br.com.rodz.input;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public ResponseEntity<?> helloMethod() {
+        return ResponseEntity.ok("Olá");
+    }
+
+}
